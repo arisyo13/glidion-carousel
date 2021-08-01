c45c365c90142135f663eaa1727995f7fe25ac15
arisyo13 <51513565+arisyo13@users.noreply.github.com>
2021-08-01 13:35:57 +0200

    ref for slider and more styling

--- src/conmponents/Carousel/index.tsx
+++ src/conmponents/Carousel/index.tsx
@@ -1,21 +1,63 @@
 import { useState } from 'react';
+import { getDays } from './definitions';
+import arrow from './assets/arrow.svg';
 import Slider from 'react-slick';
 import DateCard from '../DateCard';
-import { getDays } from './definitions';
 
 const settings = {
+    infinite: false,
     arrows: true,
     speed: 500,
-    slidesToShow: 4,
+    slidesToShow: 7,
     slidesToScroll: 7,
+    initialSlide: 0,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+            }
+        }
+    ]
 }
 
 const Carousel = () => {
+    let slider: Slider | null = null;
     const dayList = getDays(14);
-    const [selectedDateIndex, setSelectedDateIndex] = useState(0)
+    const [selectedDateIndex, setSelectedDateIndex] = useState(0);
+
+	const NextArrow = () => (
+		<div
+			className="arrow next"
+			onClick={() => !!slider && slider.slickNext()}
+			onKeyUp={() => !!slider && slider.slickNext()}
+			role="button"
+			tabIndex={-1}
+		>
+			<img src={arrow} alt="next" />
+		</div>
+	);
+
+	const PrevArrow = () => (
+		<div
+			className="arrow prev"
+			onClick={() => !!slider && slider.slickPrev()}
+			onKeyUp={() => !!slider && slider.slickPrev()}
+			role="button"
+			tabIndex={-1}
+		>
+			<img src={arrow} alt="prev" />
+		</div>
+	);
     return (
         <div className="carousel">
-            <Slider {... { settings }}>
+            <Slider 
+                ref={a => {
+                    slider = a;
+                }}
+                {... { settings, nextArrow: <NextArrow />, prevArrow: <PrevArrow /> }}
+            >
                 { dayList.map((day, index) => (
                     <DateCard
                         key={index}
--- src/conmponents/DateCard/index.tsx
+++ src/conmponents/DateCard/index.tsx
@@ -4,14 +4,15 @@ type Props = {
     day: Date;
     size: 'small' | 'full';
     onClick: () => void;
+    disabled?: boolean;
 }
 
-const DateCard = ({ day, onClick, size }: Props) => {
-    const currentDay = format(day, 'dd MMM')
+const DateCard = ({ day, onClick, size, disabled }: Props) => {
+    const currentDay = format(day, 'dd MMM');
     return (
         <div 
             onClick={() => onClick()}
-            className={`date-card ${size === "full" && "full"}`}
+            className={`date-card ${size === "full" && "full"} ${disabled && "disabled"}`}
         >
             <h3>{currentDay}</h3>
         </div>
